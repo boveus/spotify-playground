b233d92cac9806423664a55e603223287afe3fa6
Brandon Stewart <boveus@gmail.com>
2019-11-16 11:01:44 -0500

    add new logic for tempo, add reference for tempo calculation

--- app/services/hue_api_service.rb
+++ app/services/hue_api_service.rb
@@ -9,33 +9,31 @@ module HueApiService
   end
 
   def all_lights_modify(state)
-    state[:transitiontime] = 0
-    HTTParty.put("#{HueApiService.root_url}/groups/2/action", body: state.to_json)
+    # state[:transitiontime] = 0
+    HTTParty.put("#{root_url}/groups/2/action", body: state.to_json)
   end
 
   def all_lights_flash(tempo)
-    beat = 6.0 / (tempo)
-    20.times do
-      all_lights_turn_off
-      sleep beat
-      all_lights_turn_on
-      all_lights_set_brightness(254)
-      # sleep 0.5
+    # http://bradthemad.org/guitar/tempo_explanation.php
+    beat = 240.0 / (tempo)
+    Thread.new do
+      30.times do
+        all_lights_turn_off
+        all_lights_turn_on
+        all_lights_set_brightness(254)
+      end
     end
   end
 
   def all_lights_set_brightness(amount)
-    @bri = amount
     all_lights_modify({"bri": amount})
   end
 
   def all_lights_turn_off
-    @on = false
     all_lights_modify({"on":false})
   end
 
   def all_lights_turn_on
-    @on = true
     all_lights_modify({"on":true})
   end
 
